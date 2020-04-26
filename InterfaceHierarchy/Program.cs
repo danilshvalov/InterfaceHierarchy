0925001695f33e1befbebe792464a38574d32fe9
Danil Shvalov <shvalovich@gmail.com>
2020-04-26 15:41:29 +1000

    Добавьте файлы проекта.

---
+++ InterfaceHierarchy/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceHierarchy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BitmapImage bmp = new BitmapImage();
+            bmp.Draw();
+            bmp.DrawInBoundingBox(1, 1, 1, 1);
+            bmp.DrawUpsideDown();
+
+            Console.ReadLine();
+        }
+    }
+}
